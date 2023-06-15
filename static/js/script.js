d1e978e970ccbaff947eb15c72fc8309a075107b
zikrikn <zikrikn@student.telkomuniversity.ac.id>
2023-06-15 17:57:34 +0700

    add new script

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,24 @@
+function signIn() {
+  // Lakukan proses autentikasi pengguna di sini
+  const emailInput = document.getElementById("email-input").value;
+  const passwordInput = document.getElementById("password-input").value;
+
+  // Contoh sederhana autentikasi dengan email dan password tertentu
+  if (emailInput === "user@example.com" && passwordInput === "password") {
+    // Redirect ke halaman menu.html setelah berhasil login
+    window.location.href = "menu.html";
+  } else {
+    alert("Login failed. Please check your email and password.");
+  }
+}
+
+const loginForm = document.getElementById("login-form");
+
+loginForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+  signIn();
+});
+
 function togglePasswordVisibility() {
     var passwordInput = document.getElementById("password");
     var showButton = document.getElementById("show-button");
